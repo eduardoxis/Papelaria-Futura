cb267f81897e6fe47dc03f06436693ebaab3953a
eduardoxis <134206114+eduardoxis@users.noreply.github.com>
2026-06-11 22:56:49 -0300

    Refactor firebase-config.js for export statement

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,4 @@
-// ============================================================
 // firebase-config.js
-// ============================================================
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth }       from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
@@ -15,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db   = getFirestore(app);
 export default app;
