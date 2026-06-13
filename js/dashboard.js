ca9f4c04beb47fe4de27a19431de34117a8d36c1
eduardoxis <134206114+eduardoxis@users.noreply.github.com>
2026-06-13 14:40:38 -0300

    Add edit and PDF buttons to dashboard actions

--- js/dashboard.js
+++ js/dashboard.js
@@ -86,6 +86,16 @@ function renderizarUltimas(cotacoes) {
       <td class="col-right td-valor-col"><strong>${formatarMoeda(c.valorTotal)}</strong></td>
       <td class="td-status-actions-row">
         ${badgeStatus(c.status)}
+        <div class="td-actions-wrap td-actions-mobile">
+          <button class="btn-action btn-action--edit" onclick="window.editarCotacaoById('${c.id}')">
+            <svg viewBox="0 0 20 20" fill="currentColor"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"/></svg>
+            Editar
+          </button>
+          <button class="btn-action btn-action--pdf" onclick="window.gerarPDFById('${c.id}')">
+            <svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd"/></svg>
+            PDF
+          </button>
+        </div>
       </td>
       <td class="col-right td-actions-col">
         <div class="td-actions-wrap">
