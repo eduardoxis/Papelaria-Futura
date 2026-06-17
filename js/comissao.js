45d30d95a4dfca2b8c75acfd2251ca096c98449f
eduardoxis <134206114+eduardoxis@users.noreply.github.com>
2026-06-17 16:50:27 -0300

    Refactor comissao card layout and actions

--- js/comissao.js
+++ js/comissao.js
@@ -107,31 +107,33 @@ async function carregarListaComissoes() {
 
   container.innerHTML = comissoes.map(c => `
     <div class="comissao-card">
-      <div class="comissao-card-icon">
-        <svg viewBox="0 0 20 20" fill="currentColor">
-          <path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V8z" clip-rule="evenodd"/>
-        </svg>
+      <div class="comissao-card-top">
+        <div class="comissao-card-icon">
+          <svg viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V8z" clip-rule="evenodd"/>
+          </svg>
+        </div>
+        <div class="comissao-card-body">
+          <strong class="comissao-card-titulo">${escHtml(c.titulo)}</strong>
+          ${c.descricao ? `<p class="comissao-card-desc">${escHtml(c.descricao)}</p>` : ""}
+          <span class="comissao-card-data">Criada em ${formatarData(c.dataCriacao)}</span>
+        </div>
       </div>
-      <div class="comissao-card-body">
-        <strong class="comissao-card-titulo">${escHtml(c.titulo)}</strong>
-        ${c.descricao ? `<p class="comissao-card-desc">${escHtml(c.descricao)}</p>` : ""}
-        <span class="comissao-card-data">Criada em ${formatarData(c.dataCriacao)}</span>
-      </div>
+      <div class="comissao-card-divider"></div>
       <div class="comissao-card-actions">
-        <button class="btn-icon-label btn-icon-label--view" data-action="abrir" data-id="${escHtml(c.id)}" title="Visualizar">
+        <button class="btn-icon-label btn-icon-label--view" data-action="abrir" data-id="${escHtml(c.id)}">
           <svg viewBox="0 0 20 20" fill="currentColor"><path d="M10 12a2 2 0 100-4 2 2 0 000 4z"/><path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"/></svg>
           Abrir
         </button>
-        <button class="btn-icon-label btn-icon-label--pdf" data-action="pdf" data-id="${escHtml(c.id)}" title="PDF">
-          <svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3H7a1 1 0 100 2h2v1a1 1 0 102 0v-1h2a1 1 0 100-2h-2V8z" clip-rule="evenodd"/></svg>
+        <button class="btn-icon-label btn-icon-label--pdf" data-action="pdf" data-id="${escHtml(c.id)}">
+          <svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd"/></svg>
           PDF
         </button>
-        <button class="btn-icon-label btn-icon-label--edit" data-action="editar" data-id="${escHtml(c.id)}" title="Editar">
+        <button class="btn-icon-label btn-icon-label--edit" data-action="editar" data-id="${escHtml(c.id)}">
           <svg viewBox="0 0 20 20" fill="currentColor"><path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"/></svg>
           Editar
         </button>
-        <button class="btn-icon-label btn-icon-label--delete" data-action="excluir-planilha"
-          data-id="${escHtml(c.id)}" data-titulo="${escHtml(c.titulo)}" title="Excluir">
+        <button class="btn-icon-label btn-icon-label--delete" data-action="excluir-planilha" data-id="${escHtml(c.id)}" data-titulo="${escHtml(c.titulo)}">
           <svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"/></svg>
           Excluir
         </button>
