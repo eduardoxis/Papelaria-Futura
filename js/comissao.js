7d02fd54107ccce94bbd8b21fab70823905db82e
eduardoxis <134206114+eduardoxis@users.noreply.github.com>
2026-06-18 08:43:51 -0300

    Replace PDF button SVG with new design

--- js/comissao.js
+++ js/comissao.js
@@ -126,7 +126,7 @@ async function carregarListaComissoes() {
           Abrir
         </button>
         <button class="btn-icon-label btn-icon-label--pdf" data-action="pdf" data-id="${escHtml(c.id)}">
-          <svg viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M10 2v10"/><path d="M5.5 7.5L10 12l4.5-4.5"/><path d="M4 17h12"/></svg>
+          <svg viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M6 2a2 2 0 00-2 2v12a2 2 0 002 2h8a2 2 0 002-2V7.414A2 2 0 0015.414 6L12 2.586A2 2 0 0010.586 2H6zm5 6a1 1 0 10-2 0v3.586l-1.293-1.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V8z" clip-rule="evenodd"/></svg>
           PDF
         </button>
         <button class="btn-icon-label btn-icon-label--edit" data-action="editar" data-id="${escHtml(c.id)}">
