9c58c791dcefd0e75c22be34159d9b29ca2a2c8b
eduardoxis <134206114+eduardoxis@users.noreply.github.com>
2026-07-04 18:38:08 -0300

    Add password toggle functionality in caixa.js

--- js/caixa.js
+++ js/caixa.js
@@ -669,7 +669,19 @@ function _pedirSenhaComissao(onSucesso) {
     <div class="caixa-senha-caixa">
       <h4>🔒 Senha necessária</h4>
       <p>Para lançar a comissão nesta planilha, informe a senha de acesso às cotações.</p>
-      <input type="password" id="caixaSenhaInput" class="field-input--plain" placeholder="Digite a senha" autocomplete="current-password" />
+      <div class="senha-input-wrap">
+        <input type="password" id="caixaSenhaInput" class="field-input--plain" placeholder="Digite a senha" autocomplete="current-password" />
+        <button class="btn-toggle-senha" id="caixaBtnToggleSenha" type="button" aria-label="Mostrar senha">
+          <svg id="caixaIconSenhaVis" viewBox="0 0 20 20" fill="currentColor" style="display:none">
+            <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"/>
+            <path fill-rule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clip-rule="evenodd"/>
+          </svg>
+          <svg id="caixaIconSenhaHid" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M3.707 2.293a1 1 0 00-1.414 1.414l14 14a1 1 0 001.414-1.414l-1.473-1.473A10.014 10.014 0 0019.542 10C18.268 5.943 14.478 3 10 3a9.958 9.958 0 00-4.512 1.074l-1.78-1.781zm4.261 4.26l1.514 1.515a2.003 2.003 0 012.45 2.45l1.514 1.514a4 4 0 00-5.478-5.478z" clip-rule="evenodd"/>
+            <path d="M12.454 16.697L9.75 13.992a4 4 0 01-3.742-3.741L2.335 6.578A9.98 9.98 0 00.458 10c1.274 4.057 5.064 7 9.542 7 .847 0 1.669-.105 2.454-.303z"/>
+          </svg>
+        </button>
+      </div>
       <div id="caixaSenhaErro" class="caixa-senha-erro" style="display:none"></div>
       <div class="caixa-senha-acoes">
         <button class="btn-ghost" id="caixaSenhaCancelar" type="button">Cancelar</button>
@@ -684,6 +696,14 @@ function _pedirSenhaComissao(onSucesso) {
   const fechar = () => overlay.remove();
   overlay.querySelector("#caixaSenhaCancelar").onclick = fechar;
   overlay.addEventListener("click", (e) => { if (e.target === overlay) fechar(); });
+
+  // Mostrar/ocultar senha
+  overlay.querySelector("#caixaBtnToggleSenha").addEventListener("click", () => {
+    const isPass = input.type === "password";
+    input.type = isPass ? "text" : "password";
+    overlay.querySelector("#caixaIconSenhaVis").style.display = isPass ? "" : "none";
+    overlay.querySelector("#caixaIconSenhaHid").style.display = isPass ? "none" : "";
+  });
 
   const confirmar = async () => {
     const senha  = input.value.trim();
